f6a9f5592c03bfa43cf4ad644ab1cb8ce9896311
Wang Yefu <yefuwang@microsoft.com>
2023-10-12 17:02:34 +0800

    implement xCreate

--- src/vector_vtab.cpp
+++ src/vector_vtab.cpp
@@ -1,13 +1,41 @@
 #include "vector_vtab.h"
+
+#include <charconv>
+#include <optional>
+#include <string_view>
+
 #include "sqlite3ext.h"
 
 extern const sqlite3_api_routines* sqlite3_api;
 
 namespace sqlite_vector {
 
-int Create(sqlite3* db, void* pAux, int argc, char* const* argv,
-           sqlite3_vtab** ppVTab, char** pzErr) {
-  sqlite3_vtab_config(db, SQLITE_VTAB_CONSTRAINT_SUPPORT, 1);
+static std::optional<size_t> ParseNumber(std::string_view s) {
+  size_t value = 0;
+  auto result = std::from_chars(s.data(), s.data() + s.size(),value);
+  if (result.ec == std::errc::invalid_argument || result.ec == std::errc::result_out_of_range) {
+    return std::nullopt;
+  }
+
+  return value;
+}
+
+int VectorVTable::Create(sqlite3* db, void* pAux, int argc, char* const* argv,
+                         sqlite3_vtab** ppVTab, char** pzErr) {
+  int rc = sqlite3_vtab_config(db, SQLITE_VTAB_CONSTRAINT_SUPPORT, 1);
+  if (rc != SQLITE_OK) {
+    return rc;
+  }
+
+  if (argc != 2) {
+    *pzErr = sqlite3_mprintf("Expected 2 argument, got %d", argc);
+    return SQLITE_ERROR;
+  }
+
+  rc = sqlite3_declare_vtab(db, "CREATE TABLE vector(distance INTEGER hidden)");
+  if (rc != SQLITE_OK) {
+    return rc;
+  }
 
   *ppVTab = new VectorVTable(db, 3, 1000000);
   return SQLITE_OK;
--- src/vector_vtab.h
+++ src/vector_vtab.h
@@ -1,5 +1,5 @@
-#include <set>
 #include <memory>
+#include <set>
 
 #include "hnswlib/hnswlib.h"
 #include "macros.h"
@@ -8,13 +8,16 @@
 
 namespace sqlite_vector {
 
+struct VTableColumn {
+  std::string name;
+};
+
 // Note there shouldn't be any virtual functions in this class.
 // Because VectorVTable* is expected to be static_cast-ed to sqlite3_vtab*.
 class VectorVTable : public sqlite3_vtab {
  public:
   VectorVTable(sqlite3* db, size_t dim, size_t max_elements)
-      : db_(db),
-        space_(std::make_unique<hnswlib::L2Space>(hnswlib::L2Space(dim))),
+      : space_(std::make_unique<hnswlib::L2Space>(hnswlib::L2Space(dim))),
         index_(std::make_unique<hnswlib::HierarchicalNSW<float>>(
             space_.get(), max_elements)) {
     SQLITE_VECTOR_ASSERT(db != nullptr);
@@ -22,14 +25,15 @@ class VectorVTable : public sqlite3_vtab {
     SQLITE_VECTOR_ASSERT(index_ != nullptr);
   }
 
+  int dimension() const { return space_->get_data_size(); }
+
+  static int Create(sqlite3* db, void* pAux, int argc, char* const* argv,
+                    sqlite3_vtab** ppVTab, char** pzErr);
+
  private:
-  sqlite3* db_;
   std::unique_ptr<hnswlib::SpaceInterface<float>> space_;
   std::unique_ptr<hnswlib::HierarchicalNSW<float>> index_;
   std::set<int64_t> rowids;
 };
-
-int Create(sqlite3* db, void* pAux, int argc, char* const* argv,
-           sqlite3_vtab** ppVTab, char** pzErr);
 
 }  // end namespace sqlite_vector
