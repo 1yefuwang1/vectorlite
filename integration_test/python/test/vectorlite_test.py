46726bbfee6533c1b9d1510cdbdd1590f4657e7d
Yefu Wang <1yefuwang1@gmail.com>
2024-06-20 00:04:20 +0800

    add python tests

---
+++ integration_test/python/test/vectorlite_test.py
@@ -0,0 +1,43 @@
+import vectorlite_py
+import apsw
+import pytest
+import numpy as np
+
+
+@pytest.fixture(scope='module')
+def conn() -> None:
+    conn = apsw.Connection(':memory:')
+    conn.enable_load_extension(True)
+    conn.load_extension(vectorlite_py.vectorlite_path())
+    return conn
+
+DIM = 32
+NUM_ELEMENTS = 1000
+
+# Generating sample data
+@pytest.fixture(scope='module')
+def random_vectors():
+    return np.float32(np.random.random((NUM_ELEMENTS, DIM)))
+
+def test_vectorlite_info(conn):
+    cur = conn.cursor()
+    cur.execute('select vectorlite_info()')
+    output = cur.fetchone()
+    assert f'vectorlite extension version {vectorlite_py.__version__}' in output[0]
+
+def test_l2_space_with_default_hnsw_param(conn, random_vectors):
+    cur = conn.cursor()
+    cur.execute('create virtual table x using vectorlite(my_embedding(32, "l2"), hnsw(max_elements=1000))')
+
+    for i in range(NUM_ELEMENTS):
+        cur.execute('insert into x (rowid, my_embedding) values (?, ?)', (i, random_vectors[i].tobytes()))
+    
+    result = cur.execute('select my_embedding from x where rowid = 0').fetchone()
+    assert result[0] == random_vectors[0].tobytes()
+
+    cur.execute('delete from x where rowid = 0')
+    result = cur.execute('select my_embedding from x where rowid = 0').fetchone()
+    assert result is None
+
+    result = cur.execute('select rowid, distance from x where knn_search(my_embedding, knn_param(?, ?))', (random_vectors[0].tobytes(), 10)).fetchall()
+    assert len(result) == 10
--- vectorlite_py/__init__.py
+++ vectorlite_py/__init__.py
@@ -1,5 +1,7 @@
 import os
 
+__version__ = '0.1.0'
+
 def vectorlite_path():
     loadable_path = os.path.join(os.path.dirname(__file__), 'libvectorlite')
     return os.path.normpath(loadable_path)
